82462885b2031768392b3351c22edef7d4a4ee97
George Sequeira <george.m.sequeira@gmail.com>
2014-08-24 21:38:36 -0400

    End of chapter 4

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,10 +3,16 @@ require 'spec_helper'
 describe "Static pages" do
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_title(title)}
+    it { should have_selector("h1", text: content)}
+  end
   describe "Contact" do
     before { visit contact_path }
-    it { should have_content("Contact Us") }
-    it { should have_title(full_title('Contact')) }
+    let (:title) {"Contact"}
+    let (:content) {"Contact"}
+    it_should_behave_like "all static pages"
+    it { should_not have_title('| Home')}
   end
 
   describe "Home page" do
